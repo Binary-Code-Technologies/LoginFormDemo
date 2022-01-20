63fe9a450be6f13cb1d2a5ae869345c93fc4f5eb
Jaspreet Singh Kohli <70095662+Jaspreetkohli72@users.noreply.github.com>
2022-01-20 13:21:52 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,5 @@
+function grabFormItem() {
+	var name = document.getElementById("uName").value;
+	var password = document.getElementById("password").value;
+	alert("Name: " + name + "\nPassword: " + password);
+}
